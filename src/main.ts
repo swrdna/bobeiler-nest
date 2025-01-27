6748eff3a2f426251720c6b1dca329e77302c470
Nano <swrdna@hotmail.com>
2025-01-27 19:33:55 +0800

    Add varibale APP_PORT to define api port

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from '@/app.module';
 const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.APP_PORT ?? 3000);
 };
 
 bootstrap();
